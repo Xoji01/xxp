4169d105f6963f2f9bcd950faf0eeeb6624dee07
Xoji01 <135627056+Xoji01@users.noreply.github.com>
2023-09-14 21:16:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let name = 'Alex'
+let money = 10000
+let account = 7777
+ let aa = prompt('Как вас зовут?').trim().toLowerCase()
+ if (aa === 'alex') 
+    aas = prompt ('Код баланса')
+    else 
+    alert("Не правильно")
+    
+
+if (aas === '7777') {aaf =+prompt("Сколько обналичить?")} else {alert ('Не правильно')}
+if (aaf >=0   && aaf<=10000) { aq=alert ('Все отлично')
+ 
+alert('Вы обналичиловали ' + aaf)
+alert('У вас осталось ' +('10000' -aaf))
+} else{ alert ('У вас недостаточно средтсв')}
+
